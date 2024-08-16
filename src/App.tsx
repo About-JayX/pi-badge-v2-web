6fd7c1bf91a3a7a260a81e15df0e4b7f083cdc0f
s3042HJX <2038392181@qq.com>
2024-08-16 17:53:20 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,60 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import Header from '@/components/header'
-import Router from '@/router'
+import Header from "@/components/header";
+import Router from "@/router";
 
-import { bindPidAPI, findPidAPI } from './axios/api'
-import BgAnimation from './components/animation/bg'
-import Message from './components/message'
-import PiModal from './components/piModal'
-import { useStoreDispatch, useStoreSelector } from './hook'
-import useInitialize from './hook/initialize'
-import { updatePidKey, updatePiUser } from './store/ethers'
+import { bindPidAPI, findPidAPI } from "./axios/api";
+import BgAnimation from "./components/animation/bg";
+import Message from "./components/message";
+import PiModal from "./components/piModal";
+import { useStoreDispatch, useStoreSelector } from "./hook";
+import useInitialize from "./hook/initialize";
+import { updatePidKey, updatePiUser } from "./store/ethers";
 
 export default function App() {
-  const [open, setOpen] = useState(false)
-  useInitialize()
-  const dispatch = useStoreDispatch()
-  const { pidKey, piUser } = useStoreSelector(state => state.ethers)
+  const [open, setOpen] = useState(false);
+  useInitialize();
+  const dispatch = useStoreDispatch();
+  const { pidKey, piUser } = useStoreSelector((state) => state.ethers);
   const signPiBrowser = async () => {
-    const token = location.pathname.replace('/', '')
-    const result: any = token ? await findPidAPI() : ''
-    const scopes = ['payments', 'username']
-    const authResponse = await window.Pi.authenticate(scopes, () => {})
-    dispatch(updatePiUser({ ...authResponse }))
-    dispatch(updatePidKey(result ? result.pId : result))
+    const token = location.pathname.replace("/", "");
+    const scopes = ["payments", "username"];
+    const authResponse = await window.Pi.authenticate(scopes, () => {});
+    const result: any = token ? await findPidAPI() : "";
+    dispatch(updatePiUser({ ...authResponse }));
+    dispatch(updatePidKey(result ? result.pId : result));
     !result &&
       authResponse &&
       authResponse.user &&
       authResponse.user.uid &&
-      setOpen(true)
-  }
+      setOpen(true);
+      alert(authResponse.user.uid)
+  };
 
   const getBind = async () => {
     if (piUser && piUser.user && piUser.user.uid && !pidKey) {
       try {
-        const result: any = await bindPidAPI({ pid: piUser.user.uid })
+        const result: any = await bindPidAPI({ pid: piUser.user.uid });
         if (result.success) {
-          const res: any = await findPidAPI()
-          dispatch(updatePidKey(res ? res.pId : res))
+          const res: any = await findPidAPI();
+          dispatch(updatePidKey(res ? res.pId : res));
         } else {
-          alert(JSON.stringify(result))
+          alert(JSON.stringify(result));
         }
       } catch (error) {
-        alert(JSON.stringify(error) + ' error')
+        alert(JSON.stringify(error) + " error");
       }
-      setOpen(false)
+      setOpen(false);
     }
-  }
+  };
   useEffect(() => {
-    signPiBrowser()
-  }, [])
+    signPiBrowser();
+  }, []);
   return (
     <>
       <PiModal
         open={open}
-        setWalletOpen={bool => setOpen(bool)}
+        setWalletOpen={(bool) => setOpen(bool)}
         bind={getBind}
       />
       <Message />
@@ -63,5 +64,5 @@ export default function App() {
         <Router />
       </main>
     </>
-  )
+  );
 }
